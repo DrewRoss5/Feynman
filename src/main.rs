d8f11bdcfea0effa3487f61997b2b567cb9a02ab
Drew <145530440+DrewRoss5@users.noreply.github.com>
2025-01-24 20:26:57 -0700

    Implement tokenization
    This approach is far closer to an interpreted programming language, which this calculator aims to emulate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use calc::SyntaxTree;
+use calc::Interpreter;
 
 mod calc;
 
 fn main() {
-    
+    let mut calculator = Interpreter::new();
+    match calculator.evaluate("11 - 6 * 2".to_string()){
+        Ok(val) => {println!("Value: {}", val)}
+        Err(err) => {println!("Err: {}", err)}
+    }
 }
